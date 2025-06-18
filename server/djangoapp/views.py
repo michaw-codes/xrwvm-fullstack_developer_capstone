bf16a009ce760272cf536460a48362bb780f6341
michaw-codes <111310362+michaw-codes@users.noreply.github.com>
2025-06-18 15:35:11 +0200

    Update views.py

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -128,6 +128,11 @@ def add_review(request):
             response = post_review(data)
             return JsonResponse({"status":200})
         except:
-            return JsonResponse({"status":401,"message":"Error in posting review"})
+            return JsonResponse(
+                {
+                    "status": 401,
+                    "message": "Error in posting review"
+                }
+            )
     else:
-        return JsonResponse({"status":403,"message":"Unauthorized"})
+        return JsonResponse({"status": 403, "message": "Unauthorized"})
