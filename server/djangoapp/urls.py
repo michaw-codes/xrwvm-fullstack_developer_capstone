b6f33581dcb1217f99d5e8820bccebdcee9bb19d
michaw-codes <111310362+michaw-codes@users.noreply.github.com>
2025-06-18 15:32:53 +0200

    Update urls.py

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -16,19 +16,31 @@ urlpatterns = [
     path(route='logout', view=views.logout_request, name='logout'),
 
     # path for get_cars view
-    path(route='get_cars', view=views.get_cars, name ='getcars'),
+    path(route='get_cars', view=views.get_cars, name='getcars'),
 
     # path for get_dealerships view
     path(route='get_dealers', view=views.get_dealerships, name='get_dealers'),
 
     # path for get_dealers_by_state view
-    path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
-    
+    path(
+        route='get_dealers/<str:state>',
+        view=views.get_dealerships,
+        name='get_dealers_by_state'
+    ),
+
     # path for get_dealer_details view
-    path(route='dealer/<int:dealer_id>', view=views.get_dealer_details, name='dealer_details'),
-    
+    path(
+        route='dealer/<int:dealer_id>',
+        view=views.get_dealer_details,
+        name='dealer_details'
+    ),
+
     # path for get_dealer_reviews view
-    path(route='reviews/dealer/<int:dealer_id>', view=views.get_dealer_reviews, name='dealer_details'),
+    path(
+        route='reviews/dealer/<int:dealer_id>',
+        view=views.get_dealer_reviews,
+        name='dealer_details'
+    ),
 
     # path for add a review view
     path(route='add_review', view=views.add_review, name='add_review'),
